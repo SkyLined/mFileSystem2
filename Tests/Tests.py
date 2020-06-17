879fd8a6f8bd3dea6421387950238bde81c30880
SkyLined <github@skylined.nl>
2020-06-17 17:45:39 +0200

    Dependency testing

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -1,49 +1,53 @@
-import os, sys;
+from fTestDependencies import fTestDependencies;
+fTestDependencies();
 
-# Augment the search path to make cBugId a package and have access to its modules folder.
-sTestsFolderPath = os.path.dirname(os.path.abspath(__file__));
-sMainFolderPath = os.path.dirname(sTestsFolderPath);
-sParentFolderPath = os.path.dirname(sMainFolderPath);
-sModulesFolderPath = os.path.join(sMainFolderPath, "modules");
-asOriginalSysPath = sys.path[:];
-sys.path = [sParentFolderPath, sModulesFolderPath] + asOriginalSysPath;
-# Save the list of names of loaded modules:
-asOriginalModuleNames = sys.modules.keys();
-
-import mFileSystem2;
-
-# Sub-packages should load all modules relative, or they will end up in the global namespace, which means they may get
-# loaded by the script importing it if it tries to load a differnt module with the same name. Obviously, that script
-# will probably not function when the wrong module is loaded, so we need to check that we did this correctly.
-asUnexpectedModules = list(set([
-  sModuleName.lstrip("_").split(".", 1)[0] for sModuleName in sys.modules.keys()
-  if not (
-    sModuleName in asOriginalModuleNames # This was loaded before
-    or sModuleName.lstrip("_").split(".", 1)[0] in [
-      "mFileSystem2",
-      # These packages are loaded by mFileSystem2:
-      "mWindowsSDK",
-      # These built-in modules are loaded by these packages:
-      "binascii", "bz2", "cStringIO", "collections", "ctypes", "fnmatch", "gc",
-      "heapq", "io", "itertools", "keyword", "msvcrt", "platform", "shutil",
-      "string", "strop", "struct", "subprocess", "thread", "threading", "time", 
-      "zipfile", "zlib"
-    ]
-  )
-]));
-assert len(asUnexpectedModules) == 0, \
-      "Module(s) %s was/were unexpectedly loaded!" % ", ".join(sorted(asUnexpectedModules));
-
-from fTestFileFunctions import fTestFileFunctions;
-from fTestFolderFunctions import fTestFolderFunctions;
-from fTestZipFileFunctions import fTestZipFileFunctions;
-
-sTempFolderPath = os.environ["TEMP"];
-assert mFileSystem2.fbIsFolder(sTempFolderPath), \
-    "Cannot find %s" % sTempFolderPath;
-
-print "* Running tests...";
-fTestFileFunctions();
-fTestFolderFunctions();
-fTestZipFileFunctions();
-print "+ Done.";
+try:
+  import mDebugOutput;
+except:
+  mDebugOutput = None;
+try:
+  try:
+    from oConsole import oConsole;
+  except:
+    import sys, threading;
+    oConsoleLock = threading.Lock();
+    class oConsole(object):
+      @staticmethod
+      def fOutput(*txArguments, **dxArguments):
+        sOutput = "";
+        for x in txArguments:
+          if isinstance(x, (str, unicode)):
+            sOutput += x;
+        sPadding = dxArguments.get("sPadding");
+        if sPadding:
+          sOutput.ljust(120, sPadding);
+        oConsoleLock.acquire();
+        print sOutput;
+        sys.stdout.flush();
+        oConsoleLock.release();
+      fPrint = fOutput;
+      @staticmethod
+      def fStatus(*txArguments, **dxArguments):
+        pass;
+  
+  import os;
+  
+  import mFileSystem2;
+  from fTestFileFunctions import fTestFileFunctions;
+  from fTestFolderFunctions import fTestFolderFunctions;
+  from fTestZipFileFunctions import fTestZipFileFunctions;
+  
+  sTempFolderPath = os.environ["TEMP"];
+  assert mFileSystem2.fbIsFolder(sTempFolderPath), \
+      "Cannot find %s" % sTempFolderPath;
+  
+  print "* Running tests...";
+  fTestFileFunctions();
+  fTestFolderFunctions();
+  fTestZipFileFunctions();
+  print "+ Done.";
+  
+except Exception as oException:
+  if mDebugOutput:
+    mDebugOutput.fTerminateWithException(oException, bShowStacksForAllThread = True);
+  raise;
